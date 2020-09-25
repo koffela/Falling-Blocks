c9e5cbfa846b1fa18bf3dbfe85db672787c5b2c6
Aaron Koffel <amkoffel@gmail.com>
2020-09-25 16:32:16 -0700

    initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	//set the speed of the player
+	public float speed = 7;
+
+	float screenHalfWidthInWorldUnits;
+    // Start is called before the first frame update
+    void Start()
+    {
+		//since the player width needs to be taken into account when going off screen...
+		float halfPlayerWidth = transform.localScale.x / 2f;
+		//set the width to be camera size orthographic size
+		screenHalfWidthInWorldUnits = Camera.main.aspect * Camera.main.orthographicSize + halfPlayerWidth;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		float inputX = Input.GetAxisRaw("Horizontal");
+		float velocity = inputX * speed;
+		transform.Translate(Vector2.right * velocity * Time.deltaTime);
+
+		//if the player goes too far left, make it appear on the right
+		if (transform.position.x < -screenHalfWidthInWorldUnits)
+		{
+			transform.position = new Vector2(screenHalfWidthInWorldUnits, transform.position.y);
+		}
+		//if the player goes too far right, make them appear on the left
+		if (transform.position.x > screenHalfWidthInWorldUnits)
+		{
+			transform.position = new Vector2(-screenHalfWidthInWorldUnits, transform.position.y);
+		}
+	}
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+	//get access to the prefab
+	public GameObject fallingBlockPrefab;
+	//create a timer between spawns
+	public float secondsBetweenSpawns = 1;
+	//next spawn time starts at zero?
+	float nextSpawnTime;
+
+	Vector2 screenHalfSizeWorldUnits;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		//get world size (x, y)
+		screenHalfSizeWorldUnits = new Vector2(Camera.main.aspect * Camera.main.orthographicSize, Camera.main.orthographicSize);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		//if the time that's passed is greater that the next spawn time (starting at zero?) then 
+		if (Time.time > nextSpawnTime)
+		{
+			//the next block will spawn at 1 second
+			nextSpawnTime = Time.time + secondsBetweenSpawns;
+			Vector2 spawnPosition = new Vector2(Random.Range(-screenHalfSizeWorldUnits.x, screenHalfSizeWorldUnits.x), screenHalfSizeWorldUnits.y + spawnSize/2f);
+			Instantiate(fallingBlockPrefab, spawnPosition, Quaternion.identity);
+		}
+    }
+}
